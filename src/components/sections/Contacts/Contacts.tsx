63014c6376a32cf7e525b2783ba5ead52725fa51
yanakorobova <08mickey01@gmail.com>
2023-04-11 22:51:06 +0300

    feat: added location

--- src/components/sections/Contacts/Contacts.tsx
+++ src/components/sections/Contacts/Contacts.tsx
@@ -3,11 +3,11 @@ import s from './Contacts.module.scss'
 import {Form} from "./Form/Form";
 import {Title} from "../../common/Title/Title";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faEnvelopeOpen, faSquarePhone} from "@fortawesome/free-solid-svg-icons"
+import {faEnvelopeOpen, faSquarePhone,faLocationDot} from "@fortawesome/free-solid-svg-icons"
 
 export const Contacts = () => {
     return (
-        <div className={s.contactsBlock}>
+        <div className={s.contactsBlock} id={'contacts'}>
             <div className={s.section}>
                 <div className={s.container}>
                     <Title title={'GET IN TOUCH.'} titleBg={'CONTACT'}/>
@@ -19,19 +19,25 @@ export const Contacts = () => {
                                 creative ideas or opportunities to be part of your visions.
                             </p>
                             <p className={s.contact}>
-                                <FontAwesomeIcon icon={faEnvelopeOpen} className={s.mail}/>
-                                <span className={s.block}>
-                                <span className={s.bText}>MAIL</span><br/>
-                                <span>yanaheaven@yandex.ru</span>
-
-                            </span>
+                                <FontAwesomeIcon icon={faEnvelopeOpen} className={s.icon}/>
+                                <div className={s.block}>
+                                    <span className={s.bText}>MAIL</span><br/>
+                                    yanaheaven@yandex.ru
+                                </div>
                             </p>
                             <p className={s.contact}>
-                                <FontAwesomeIcon icon={faSquarePhone} className={s.phone}/>
-                                <span className={s.block}>
-                                <span className={s.bText}>PHONE</span><br/>
-                                 8-904-330-40-66
-                            </span>
+                                <FontAwesomeIcon icon={faSquarePhone} className={s.icon}/>
+                                <div className={s.block}>
+                                    <span className={s.bText}>PHONE</span><br/>
+                                    +7 (904) 330-40-66
+                                </div>
+                            </p>
+                            <p className={s.contact}>
+                                <FontAwesomeIcon icon={faLocationDot} className={s.icon}/>
+                                <div className={s.block}>
+                                    <span className={s.bText}>LOCATION</span><br/>
+                                    St.Petersburg, Russia
+                                </div>
                             </p>
                         </div>
                         <Form/>
