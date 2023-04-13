0927c0ce1132b1ac66d514d4149407518ac55e5c
yanakorobova <08mickey01@gmail.com>
2023-04-13 16:45:01 +0300

    feat: updated intro

--- src/components/sections/Intro/Intro.tsx
+++ src/components/sections/Intro/Intro.tsx
@@ -1,22 +1,37 @@
 import React from 'react';
-import s from 'components/sections/Intro/Intro.module.css'
-import c from '../../../assets/styles/commonStyles.module.css'
+import s from 'components/sections/Intro/Intro.module.scss'
+import {Button} from "components/common/Button/Button";
+import {faDownload} from "@fortawesome/free-solid-svg-icons";
+import photo from 'assets/images/photo.jpg'
+
 
 export const Intro = () => {
     return (
-        <div className={s.mainBlock}>
-            <div className={c.section}>
-                <div className={c.container}>
-                    <div className={s.containerText}>
-                        <span>Welcome to my Portfolio</span>
-                        <h2>
-                            Hi, I'm
-                            <span> Yana</span>
-                        </h2>
-                        <span>Frontend Developer</span>
-                        <p className={s.text}>asddasdasdddddddddd asdasdasd asdasdasd asdqweqwe asdeqwe asdasdewewe</p>
+        <div className={s.mainBlock} id={'intro'}>
+            <div className={s.section}>
+                <div className={s.container}>
+                    <div className={s.content}>
+                        <div className={s.containerText}>
+                            <p className={s.title}>Welcome to my Portfolio</p>
+                            <div className={s.description}>
+                                <h1>
+                                    Hello, I'm <p>Yana.</p>
+                                </h1>
+                                <p className={s.text}>I'm a front-end developer with experience in creating SPA, using
+                                    React/Redux/TypeScript. Now I am improving my skills in this direction and expanding
+                                    them with new technologies. Open to your suggestions!</p>
+                            </div>
+
+                            <a href={'CV'} download>
+                                <Button type={'button'} iconType={faDownload}>DOWNLOAD CV</Button>
+                            </a>
+                        </div>
+
+                        <div className={s.photo}>
+                            <img src={photo} alt={'photo'}/>
+                        </div>
                     </div>
-                    <div className={s.photo}></div>
+
                 </div>
             </div>
         </div>
